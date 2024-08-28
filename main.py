1fee3eefec9cb99769796af4266be38f321b92b5
zeynep <zeynep.eminoglu@clearscore.com>
2024-08-28 21:57:57 +0100

    adding pawn functionality

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ def draw_pieces():
 def check_options(pieces,locations,turn):
     moves_list = []
     all_moves_list = []
-    for i in range((pieces)):
+    for i in range((len(pieces))):
         location = locations[i]
         piece = pieces[i]
         if piece == 'pawn':
@@ -148,6 +148,59 @@ def check_options(pieces,locations,turn):
         all_moves_list.append(moves_list)
     return all_moves_list
 
+# Function for checking valid pawn moves       
+def check_pawn(position, color):
+    moves_list = []
+    if color == 'white':
+        if (position[0], position[1] + 1) not in white_locations and \
+                (position[0], position[1] + 1) not in black_locations and position[1] < 7:
+            moves_list.append((position[0], position[1] + 1))
+        if (position[0], position[1] + 2) not in white_locations and \
+                (position[0], position[1] + 2) not in black_locations and position[1] == 1:
+            moves_list.append((position[0], position[1] + 2))
+        if (position[0] + 1, position[1] + 1) in black_locations:
+            moves_list.append((position[0] + 1, position[1] + 1))
+        if (position[0] - 1, position[1] + 1) in black_locations:
+            moves_list.append((position[0] - 1, position[1] + 1))
+    else:
+        if (position[0], position[1] - 1) not in white_locations and \
+                (position[0], position[1]  - 1) not in black_locations and position[1] > 0:
+            moves_list.append((position[0], position[1] - 1))
+        if (position[0], position[1] - 2) not in white_locations and \
+                (position[0], position[1] - 2) not in black_locations and position[1] == 6:
+            moves_list.append((position[0], position[1] - 2))
+        if (position[0] + 1, position[1] - 1) in white_locations:
+            moves_list.append((position[0] + 1, position[1] - 1))
+        if (position[0] - 1, position[1] - 1) in white_locations:
+            moves_list.append((position[0] - 1, position[1] - 1))
+    return moves_list
+
+
+# Function for checking the valid moves the selected picea can make 
+def check_valid_moves():
+    if turn_step < 2:
+        options_list = white_options
+    else:
+        options_list = black_options
+    valid_options = options_list[selection]
+    return valid_options
+
+
+
+# Function for drawing valid moves on screen
+def draw_valid(moves):
+    if turn_step < 2:
+        color = 'red'
+    else:
+        color = 'blue'
+    for i in range(len(moves)):
+        pygame.draw.circle(screen,color,(moves[i][0] * 100 + 50, moves[i][1] * 100 + 50),5 )
+
+# uu
+
+
+
+
 # THE MAIN GAME LOOP
 black_options = check_options(black_pieces,black_locations, 'black')
 white_options = check_options(white_pieces, white_locations, 'white')
@@ -157,6 +210,10 @@ while run:
     screen.fill('dark gray')
     draw_board()
     draw_pieces()
+    if selection != 100:
+        valid_moves = check_valid_moves()
+        draw_valid(valid_moves)
+
 # Event Handling 
     for event  in pygame.event.get():
         # event where pygame crashes and quits 
